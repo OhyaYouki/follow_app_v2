f126393f828e5f3831234fc66603f71e57473bbc
OhyaYouki <88011584+OhyaYouki@users.noreply.github.com>
2023-03-25 21:28:48 +0900

    モデルのアソシエーション修正

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,2 +1,7 @@
 class Following < ApplicationRecord
+
+  # relationship（中間テーブル）とのアソシエーション
+  has_many :relationships
+  # userモデルとのアソシエーション
+  has_many :users            , through: :relationships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,24 +9,8 @@ class User < ApplicationRecord
 
   has_many :posts
 
-  ## Userモデルに記載のアソシエーション
-
   # relationship（中間テーブル）とのアソシエーション
-  has_many :relationships                                              , foreign_key: "user_id"   # 省略可能
-  # 架空のfollowingモデルとのアソシエーション
-  has_many :followings            , through:     :relationships        , source:      :following  # following_id参照
-
-
-  ## 架空のfollowingモデルのアソシエーション
-
-  # 架空のfollowingモデルに記載するはずのrelationship（中間テーブル）とのアソシエーション
-  has_many :passive_relationships , class_name: "Relationship"         , foreign_key: 'following_id'
-  # 本当はhas_many :usersと書きたいけど書けないからfollowersとしている
-  has_many :followers             , through:    :passive_relationships , source:      :user       # user_id参照
+  has_many :relationships
+  # followingモデルとのアソシエーション
+  has_many :followings       , through: :relationships
 end
-
-# 本当はこうしたい
-# class following < ApplicationRecord
-#   has_many :relationships         , foreign_key: 'following_id'
-#   has_many :users                 , through:    :relationships 
-# end
